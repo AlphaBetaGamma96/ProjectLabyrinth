49964c0069ab057f055597688115be188afaaf86
AlphaBetaGamma96 <jameswtkeeble@gmail.com>
2020-08-24 01:36:48 +0100

    spirte example update

--- sprite_example.py
+++ sprite_example.py
@@ -1,8 +1,8 @@
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 800
+HEIGHT = 600
 FPS = 30
 
 #COLOURS
@@ -11,6 +11,23 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+class Player(pygame.sprite.Sprite):
+    #pygame object to represent sprites
+    #inherit pygame.sprite.Sprite
+    def __init__(self):
+        super(Player, self).__init__()
+        #required attributes
+        self.image = pygame.Surface((50, 50)) #what it looks like
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()  #rectange that encloses sprite (outer border)
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+
+    def update(self):
+        self.rect.x += 5
+        if(self.rect.left>WIDTH): #if left side of rect of sprite goes pass screen
+            self.rect.right=0 #reset to 0
+
 
 #init pygame
 pygame.init()
@@ -22,6 +39,8 @@ pygame.display.set_caption("My Game")
 clock = pygame.time.Clock() #create clock to manage FPS
 
 all_sprites = pygame.sprite.Group() #creates new group (add all sprites to this)
+player = Player()
+all_sprites.add(player)
 
 #Game loop
 running = True
